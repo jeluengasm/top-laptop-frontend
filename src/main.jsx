601e76046720c654bfc31240841c4a4d17ea9fae
Jhon Luengas <jeluengasm@unal.edu.co>
2023-07-06 19:11:46 -0500

    feat(develop): Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './assets/css/5_0_2_dist_css_bootstrap.min.css'
+import './assets/js/5_0_2_dist_js_bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
